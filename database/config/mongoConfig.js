c849604e7393b62ac79e3769586540708985f2c7
Danilo Donado <danilodonadoa@gmail.com>
2024-12-18 20:55:32 -0400

    fixed production mongo uri variable

--- database/config/mongoConfig.js
+++ database/config/mongoConfig.js
@@ -16,14 +16,14 @@ class MongoConnection {
 
     async _connect() {
         try {
-            mongoose.connect(process.env.DOCKER_MONGO_URI, {
-                user: process.env.USER_MONGO,
-                pass: process.env.PASS_MONGO,
-                authMechanism: "DEFAULT",
-                authSource: "admin"
-            });  
+            // mongoose.connect(process.env.DOCKER_MONGO_URI, {
+            //     user: process.env.USER_MONGO,
+            //     pass: process.env.PASS_MONGO,
+            //     authMechanism: "DEFAULT",
+            //     authSource: "admin"
+            // });  
 
-            // mongoose.connect(process.env.FULL_DOCKER_MONGO_URI); 
+            mongoose.connect(process.env.PRODUCTION_MONGO_URI); 
 
             console.log('MongoDB connected'); 
         } 
