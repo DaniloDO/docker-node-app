a2ac31aeb5d95980b78012705ee0519310ab4c53
Danilo Donado <danilodonadoa@gmail.com>
2024-12-20 16:21:29 -0400

    fixed docker compose file to improve security, added auth capabilities to mongo and redis

--- database/config/mongoConfig.js
+++ database/config/mongoConfig.js
@@ -16,14 +16,15 @@ class MongoConnection {
 
     async _connect() {
         try {
-            // mongoose.connect(process.env.DOCKER_MONGO_URI, {
-            //     user: process.env.USER_MONGO,
-            //     pass: process.env.PASS_MONGO,
-            //     authMechanism: "DEFAULT",
-            //     authSource: "admin"
-            // });  
+            mongoose.connect(process.env.DOCKER_MONGO_URI, {
+                user: process.env.USER_MONGO,
+                pass: process.env.PASS_MONGO,
+                authMechanism: "DEFAULT",
+                authSource: "admin"
+            });  
 
-            mongoose.connect(process.env.PRODUCTION_MONGO_URI); 
+            // mongoose.connect(process.env.LOCAL_MONGO_URI); 
+            // mongoose.connect(process.env.MONGO_URI); 
 
             console.log('MongoDB connected'); 
         } 
