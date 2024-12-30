5f13c0840cd79453b6cab3d90ae64f515165a272
Danilo Donado <danilodonadoa@gmail.com>
2024-12-30 12:38:14 -0400

    testing local env variables in workflow

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ console.log('NODE_ENV:', process.env.NODE_ENV);
 console.log('CONFIRM:', process.env.CONFIRM);
 console.log('PORT:', process.env.PORT);
 
-// mongoConnection; 
+mongoConnection; 
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-// app.use('/api/user', userRouter); 
+app.use('/api/user', userRouter); 
 
 app.set('json spaces', 2); 
 
